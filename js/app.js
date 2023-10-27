65bda895ebdc877ce829c63847c53f73ab4a582d
paulohcatto@gmail.com <paulohcatto@gmail.com>
2023-10-26 21:12:40 -0300

    Criando o projeto E-Ticket

---
+++ js/app.js
@@ -0,0 +1,48 @@
+function comprar() {
+    let tipo = document.getElementById('tipo-ingresso');
+    let qtd = parseInt(document.getElementById('qtd').value);
+
+    if (tipo.value == 'pista') {
+        comprarPista(qtd);
+    } else if (tipo.value == 'superior') {
+        comprarCadeiraSuperior(qtd);
+    } else {
+        comprarCadeiraInferior(qtd);
+    }
+}
+
+function comprarPista(qtd) {
+    let pista = parseInt(document.getElementById('qtd-pista').textContent);
+
+    if (qtd > pista) {
+        alert("Quantidade indisponivel");
+    } else {
+        qtdPista = pista - qtd;
+        document.getElementById('qtd-pista').textContent = qtdPista;
+        alert('parabens, deu certo a parada');
+    }
+}
+
+function comprarCadeiraSuperior(qtd) {
+    let superior = parseInt(document.getElementById('qtd-superior').textContent);
+
+    if (qtd > superior) {
+        alert("Quantidade indisponivel");
+    } else {
+        qtdSuperior = superior - qtd;
+        document.getElementById('qtd-superior').textContent = qtdSuperior;
+        alert('Compra realizada com sucesso');
+    }
+}
+
+function comprarCadeiraInferior(qtd) {
+    let inferior = parseInt(document.getElementById('qtd-inferior').textContent);
+
+    if (qtd > inferior) {
+        alert("Quantidade indisponivel");
+    } else {
+        qtdInferior = inferior - qtd;
+        document.getElementById('qtd-inferior').textContent = qtdInferior;
+        alert('parabens, deu certo a parada');
+    }
+}
